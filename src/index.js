e9960c3e81989ea55798fd385037821755d7fb6b
peanut-dev <wmhayt228@gmail.com>
2021-06-30 07:49:28 +0800

    # init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/**
+ * Vue构造函数
+ * @param {*} options new Vue传递的配置对象
+ */
+export default function Vue(options) {
+  this._init(options)
+}
+
+/**
+ * 初始化配置对象
+ * @param {*} options 
+ */
+Vue.prototype._init = function (options) {
+  // 将options配置挂载到Vue实例上
+  this.$options = options
+  // 初始化options.data
+  // 代理data对象上的各个属性到Vue实例
+  // 给data对象上的各个属性设置响应式能力
+  initData(this)
+}
---
+++ src/initData.js
@@ -0,0 +1,17 @@
+export default function initData(vm) {
+  // 获取data选项
+  let { data } = vm.$options
+  // 设置vm.data选项，保证它的值肯定是一个对象
+  if (!data) {
+    vm._data = {}
+  } else {
+    vm._data = typeof data === 'function' ? data() : data
+  }
+  // 代理，将data对象上的各个属性代理到vue实例上，支持通过this.XXXX访问
+  for (let key in vm._data) {
+    proxy(vm, '_data', key)
+  }
+
+  // 设置响应式
+  observe(vm._data)
+}
